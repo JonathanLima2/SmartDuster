4b43a5c2f4d531f4167475387edf9eec1e04cb94
jonathanLima <jonathan.lima@eldorado.org.br>
2022-11-11 16:25:19 -0400

    adição de mudanças no código arduino

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # SmartDuster
 Projeto de desenvolvimento de um aspirador de pó inteligente. Trabalho de conclusão do curso de Desenvolvimento android Devtitans.
+
+#Parâmetros de retorno do .ino
+>["RGS",SL, SU, SB, AA, AV, RE, REFT, RD, RDFT]
+>Parâmetros comuns entre as chamadas
+0x00 - se o sensor não identificar luminosidade.
+0x01 ou mais - se o sensor identificar luminosidade.
+0xFF - ocorreu erro na comunicação com o sensor, verificar a conexão e/ou o correto funcionamento do sensor.
+
+OBS:
+Para os parâmetros REFT e RDFT só existem 2 estados(00 e 01), devido a eles dependerem da ativação da roda.
+
+>Parâmetros de retorno específicos
+>>SL(sensor de luminosidade)
+em RGS retorna
+->0x00 se tiver sucesso
+->0x01 se tiver falhado 
+>>SU(sensor Ultrassônico)
+em RGS retorna
+->0x00 se tiver sucesso
+->0x02 se tiver falhado 
+>>SB(sensor de batida)
+em RGS retorna
+->0x00 se tiver sucesso
+->0x03 se tiver falhado 
+>>AA(ativador do aspirador)
+em RGS retorna
+->0x00 se tiver sucesso
+->0x04 se tiver falhado 
+>>AV(ativador da vassoura)
+em RGS retorna
+->0x00 se tiver sucesso
+->0x05 se tiver falhado 
+>>RE(motor da Roda Esquerda)
+em RGS retorna
+->0x00 se tiver sucesso
+->0x06 se tiver falhado 
+>>RD(motor da Roda Direita)
+em RGS retorna
+->0x00 se tiver sucesso
+->0x07 se tiver falhado 
+
+>>REFT(Força de tração da roda esquerda)
+>>RDFT(Força de tração da roda direita)
--- duster_commands.ino
+++ duster_commands.ino
@@ -1,4 +1,16 @@
 #define LIGHT_SENSOR_PIN 2 // ESP32 pin GIOP36 (ADC0)
+//parâmetros de retorno
+//["RGS",SL, SU, SB, AA, AV, RE, REFT, RD, RDFT]
+byte RGS = 0x00;
+byte SL = 0x00;
+byte SU = 0x00;
+byte SB = 0x00;
+byte AA = 0x00;
+byte AV = 0x00;
+byte RE = 0x00;
+byte REFT = 0x00;
+byte RD = 0x00;
+byte RDFT = 0x00;
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -10,10 +22,24 @@ void loop() {
     delay(500);
 }
 
+byte[] getStatus(){
+  byte[9] status;
+  byte[0] = getLight(); 
+  byte[1] = getLight(); 
+  byte[2] = getLight(); 
+  byte[3] = getLight(); 
+  byte[4] = getLight(); 
+  byte[5] = getLight(); 
+  byte[6] = getLight(); 
+  byte[7] = getLight(); 
+  byte[8] = getLight(); 
+}
+
 //código referente ao status de luz do ambiente;
-string getLight(){
+byte getLight(){
     // reads the input on analog pin (value between 0 and 4095)
   int analogValue = analogRead(LIGHT_SENSOR_PIN);
+  byte ret = 0x00;
   string light_Ambient ="";
   light_Ambient += "Analog Value = ";
   light_Ambient += analogValue;   // the raw analog reading
@@ -30,39 +56,85 @@ string getLight(){
   } else {
     light_Ambient += " => Extremamente Claro";
   }
-  return light_Ambient;
+  Serial.println(light_Ambient);
+  if(analogValue>0)
+    ret = 0x01;
+  else if(analogValue==0)
+    ret = 0x00;
+  else 
+    ret = 0xff; 
+  return ret;
 }
 
 //código referente ao uso da vassoura 
-string vassoura = "";
-boolean Vstatus = false;
-string actionVassoura(){
-    Vstatus = !Vstatus;
-    if(Vstatus==true){
-        vassoura = "on";
+byte actionVassoura(){
+    if(AV==0x00){
+      AV=0x01;
     }else{
-        vassoura = "off";
+      AV=0x00;
     }
-    return vassoura;
+    return AV;
 }
 
-string statusVassoura(){
-  return Vstatus;
+byte statusVassoura(){
+  return AV;
 }
 
 //código referente ao uso do aspirador 
-string aspirador = "";
-boolean Astatus = false;
-string actionAspirador(){
-    Astatus = !Astatus;
-    if(Astatus==true){
-        aspirador = "on";
+byte actionAspirador(){
+    if(AA==0x00){
+      AA=0x01;
     }else{
-        aspirador = "off";
+      AA=0x00;
     }
-    return aspirador;
+    return AA;
 }
 
-string statusAspirador(){
-  return Astatus;
+byte statusAspirador(){
+  return AA;
+}
+
+//código referente ao uso da roda esquerda e sua tração
+byte actionRodaEsqueda(){
+    if(RE==0x00){
+      RE=0x01;
+    }else{
+      RE=0x00;
+    }
+    return RE;
+}
+
+byte statusRE(){
+  return RE;
+}
+byte TracaoEsquerda(){
+  if(RE==0x01 && REFT==0x00)
+    REFT=0x01;
+  return RE;
+}
+
+byte statusREFT(){
+  return REFT;
+}
+//código referente ao uso da roda direita e sua tração
+byte actionRodaDireita(){
+    if(RD==0x00){
+      RD=0x01;
+    }else{
+      RD=0x00;
+    }
+    return RD;
+}
+
+string statusRD(){
+  return RD;
+}
+byte TracaoDireita(){
+  if(RD==0x01 && RDFT==0x00)
+    RDFT=0x01;
+  return RE;
+}
+
+byte statusRDFT(){
+  return RDFT;
 }
